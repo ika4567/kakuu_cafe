a9e9c1acdca98c5d44c8c378131c4da41b449baa
ika4567 <yumi.wara2@gmail.com>
2022-03-16 06:38:02 +0000

    [Add] Adminサインイン､サインアウト機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,28 @@
 Rails.application.routes.draw do
-  
+
   namespace :admin do
     root to: 'homes#top'
   end
-  
+
   namespace :admin do
     get 'orders/index'
   end
-  
+
   namespace :admin do
     get 'customers/show'
   end
-  
+
   namespace :admin do
     get 'products/index'
     get 'products/sale'
     get 'products/edit'
   end
 
-  
+    scope module: :public do
+    root to: 'homes#top'
+    get '/about' => 'homes#about'
+  end
+
   scope module: :public do
     get 'orders/note'
     get 'orders/new'
@@ -26,13 +30,12 @@ Rails.application.routes.draw do
     get 'orders/index'
     get 'orders/edit'
   end
+
   scope module: :public do
-    get 'customers/show'
+    get 'customers/my_page' => 'customers#show', as: 'my_page'
     get 'customers/edit'
   end
-  scope module: :public do
-    root to: 'homes#top'
-  end
+
 
   devise_for :admin, skip: [:registrations, :passwords] , controllers: {
     sessions: "admin/sessions"
