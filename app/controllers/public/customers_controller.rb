ffd40144eff621069d36c7b218fd31885f396eaf
ika4567 <yumi.wara2@gmail.com>
2022-03-19 08:31:41 +0000

    [Add]public/customer 登録情報編集機能と退会機能を実装

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,5 +4,25 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(current_customer.id)
+  end
+  
+  def update
+    @customer = Customer.find(current_customer.id)
+    binding.pry
+    @customer.update(customer_params)
+    redirect_to my_page_path
+  end
+  
+  def cancel
+    @customer = Customer.find(current_customer.id)
+    @customer.update(is_active: true)
+    # reset_session
+    redirect_to root_path
+  end
+  
+  private
+  def customer_params
+    params.require(:customer).permit(:name, :telephone_number, :email)
   end
 end
