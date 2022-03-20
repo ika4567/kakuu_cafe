18a2afb811cac3ab430b050cf0d5dcd3ad585c22
ika4567 <yumi.wara2@gmail.com>
2022-03-20 14:48:24 +0000

    [Add]public/order 予約画面着手

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,6 +3,8 @@ class Public::OrdersController < ApplicationController
   end
 
   def new
+    @today = Date.current.strftime('%-m月%d日')
+    @on_sales = Product.where(product_status: "on_sale")
   end
 
   def thanks
