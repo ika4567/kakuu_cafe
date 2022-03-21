e6410eb63f218fb3586c6baca47e60b94277e723
ika4567 <yumi.wara2@gmail.com>
2022-03-21 13:33:08 +0000

    [Update]public/order 予約画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,6 +5,14 @@ class Public::OrdersController < ApplicationController
   def new
     @today = Date.current.strftime('%-m月%d日')
     @product = Product.where(product_status: "on_sale")
+    @order = Order.new
+    @order_detail = OrderDetail.new
+  end
+
+  def create
+    
+    @order = current_customer.orders.new
+    @order.customer_id = current_customer.id
   end
 
   def thanks
