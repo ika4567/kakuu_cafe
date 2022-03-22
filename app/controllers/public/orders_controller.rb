0e1c0a3dcdbb00d2df11bfae6a9e0a555b7af0f5
ika4567 <yumi.wara2@gmail.com>
2022-03-22 05:59:59 +0000

    [Update]public/order#new 予約機能

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,21 +6,26 @@ class Public::OrdersController < ApplicationController
     @today = Date.current.strftime('%-m月%d日')
     @product = Product.where(product_status: "on_sale")
     @order = Order.new
-    @order_detail = OrderDetail.new
+    @order.order_details.build
   end
 
   def create
     # OrderDetail
-    products = Product.where(product_status: "on_sale")
-    products.each do |product|
-      @order_detail = OrderDetail.new(order_detail_params)
-      @order_detail.order_id = current_customer.orders.id
-      @order_detail.product_id = product.id
-      @order_detail.save
-    end
+    # @products = Product.where(product_status: "discontinued")
+    # products.each do |product|
+      # @order_detail = OrderDetail.new
+    #   # @order_detail.order_id = current_customer.orders.id
+    #   # @order_detail.product_id = product.id
+    #   @order_detail.save
+    # end
     # Order
     @order = current_customer.orders.new(order_params)
     @order.save
+    # binding.pry
+    redirect_to  orders_confirm_path
+  end
+
+  def confirm
   end
 
   def thanks
@@ -34,10 +39,10 @@ class Public::OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:order).permit(:customer_id, :order_system, :time, :note)
+    params.require(:order).permit(:customer_id, :order_system, :time, :note, order_details_atributes: [:order_id, :product_id, :reservation_quantity])
   end
 
-  def order_detail_params
-    params.require(:order_detail).permit(:order_id, :product_id, :reservation_quantity)
-  end
+  # def order_detail_params
+  #   params.require(:order_detail).permit(:order_id, :product_id, :reservation_quantity)
+  # end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,10 @@
 class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
+  has_many :products, through: :order_details
+  accepts_nested_attributes_for :order_details, allow_destroy: true
+  
   belongs_to :customer
-  
+
   enum order_system: { layaway: 0, take_out: 1}
-  
+
 end
